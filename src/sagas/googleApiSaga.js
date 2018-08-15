eb1b2e8488aa97e4420430764266f039bb4d3a21
epstein.w <epstein.w@husky.neu.edu>
2018-08-15 14:37:10 -0400

    trying to get api to work

--- src/sagas/googleApiSaga.js
+++ src/sagas/googleApiSaga.js
@@ -17,11 +17,11 @@ function *  getLocation() {
     if (location) {
         console.log('successfully got location' + location.location);
 
-        const revGeo = yield call(service.getCity(location.location))
+        const revGeo = yield call(service.getCity, location.location)
         if (revGeo) {
             console.log("successfully backtraced geolocation" + revGeo.results.address_components[2]);
 
-            const finalProduct = yield call(service.setLocation(location.location.lat, location.location.lng, revGeo.results.address_components[2]));
+            const finalProduct = yield call(service.setLocation, location.location.lat, location.location.lng, revGeo.results.address_components[2]);
             if (finalProduct) {
                 console.log("saved location in DB" + finalProduct);
                 yield put({type: SET_USER_LOCATION_FULFILLED, user: finalProduct});
@@ -36,19 +36,7 @@ function *  getLocation() {
     }
 }
 
-//function * loginUser({user}) {
-//   console.log('Logging in user:', JSON.stringify(user, null, 2));
-//   const loggedIn = yield call(userService.login, user);
-//
-//   if (loggedIn) {
-//     console.log('Successfully logged in user:', JSON.stringify(loggedIn, null, 2));
-//
-//     // notify reducer
-//     yield put({type: LOGIN_USER_FULFILLED, user: loggedIn});
-//
-//     console.log('Redirecting to profile');
-//     yield put(redirect('/profile'))
-//   } else {
-//     console.error('Failed to login user');
-//   }
-// }
+export default function * rootSaga () {
+    yield all([
+        fork(takeLatest,SET_USER_LOCATION_REQUESTED, getLocation)
+    ])}
